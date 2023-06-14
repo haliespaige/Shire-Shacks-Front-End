ceacf3c7d7c8b693443a7932047f754c7cb8f08d
wabowen89 <wabowen89@gmail.com>
2023-06-14 16:35:12 -0700

    Added Home/NotFound functionality/styling/and testing

--- src/App.js
+++ src/App.js
@@ -11,14 +11,26 @@ import ShackNew from './pages/ShackNew';
 import ShackProtectedIndex from './pages/ShackProtectedIndex';
 import ShackShow from './pages/ShackShow';
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <>
       <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="*" element={<NotFound />} />
+        <Route path="/shackedit" element={<ShackEdit />} />
+        <Route path="/shackindex" element={<ShackIndex />} />
+        <Route path="/shacknew" element={<ShackNew />} />
+        <Route path="/shackprotectedindex" element={<ShackProtectedIndex />} />
+        <Route path="/shackshow" element={<ShackShow />} />
+        <Route path="/login" element={<LogIn />} />
+        <Route path="/signup" element={<SignUp />} />
+      </Routes>
       <Footer />
     </>
-  )
+  );
 }
 
 export default App;
--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,12 +1,21 @@
 import React from 'react'
+import NotFoundGIF from '../assets/NotFoundGIF.gif'
 
 const NotFound = () => {
 
   return (
     <>
       <h1>NotFound</h1>
+
+      <img
+        className="not-found-gif"
+        src={NotFoundGIF}
+        alt="aragorn lost in the forest"
+        height="200"
+      />
+
     </>
-  )
+  );
 
 }
 
